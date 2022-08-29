19774e556c181de5024d1048e65ca3f269e771c0
Rob Raddi <rraddi@temple.edu>
2022-08-29 13:10:06 -0400

    made edits to multiple files during debugging (just Py2 -> Py3 stuff)

--- hpsandbox/Chain.py
+++ hpsandbox/Chain.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python
 #
-# Chain.py 	
+# Chain.py
 #
 # HISTORY
 #     5/28/2007 - Added docstrings
@@ -17,13 +16,13 @@ import os
 #########################################################################
 
 class Chain:
-    """An object to represent the 2D HP lattice chain and its attributes, with method functions.""" 
+    """An object to represent the 2D HP lattice chain and its attributes, with method functions."""
 
     def __init__(self,config):
-        """Initialize the Chain object."""    
+        """Initialize the Chain object."""
 
         print('\tInitializing Chain.py object...')
-        self.hpstring = string.strip(config.HPSTRING)	# The HP sequence as a string
+        self.hpstring = config.HPSTRING.strip()	# The HP sequence as a string
         self.n = len(self.hpstring)	                # the chain length
         self.hpbinary = self.hpstr2bin()                # The HP seq in binary rep (H=1 P=0)
 
@@ -42,7 +41,7 @@ class Chain:
         for i in range(0,self.n-1):
             self.vec.append(config.INITIALVEC[i])       # {0,1,2,3} =  {n,w,s,e} = {U,R,D,L}
 
-        self.coords = self.vec2coords(self.vec)         # the 2D coordinates of the chain, as a list of duples 
+        self.coords = self.vec2coords(self.vec)         # the 2D coordinates of the chain, as a list of duples
         self.viable = self.viability(self.coords)       # viable chains are ones which are self-avoiding
                                                         #     1 if viable, 0 if not
 
@@ -55,24 +54,24 @@ class Chain:
         self.nextcoords = []
         for i in range(0,len(self.coords)):
             self.nextcoords.append(self.coords[i])
-    
+
         self.nextviable = self.viable
 
 
     def hpstr2bin(self):
-        """Convert a string of type 'HPHPHPPPHHP' to a list of 1s and 0s.""" 
+        """Convert a string of type 'HPHPHPPPHHP' to a list of 1s and 0s."""
         binseq = []
         for i in range(0,len(self.hpstring)):
             if self.hpstring[i] == 'H':
-                binseq.append(1)   	
+                binseq.append(1)
             else:
-                binseq.append(0)   	
+                binseq.append(0)
         return binseq
 
 
 
     def vec2coords(self,thisvec):
-        """Convert a list of chain vectors to a list of coordinates (duples).""" 
+        """Convert a list of chain vectors to a list of coordinates (duples)."""
         tmp = [(0,0)]
         x = 0
         y = 0
@@ -86,7 +85,7 @@ class Chain:
             if thisvec[i] == 3:
                 x = x - 1
             tmp.append((x,y))
-        return tmp	
+        return tmp
 
 
     def vec2str(self, vec):
@@ -119,7 +118,7 @@ class Chain:
                     if self.hpstring[d] == 'H':
                         if (abs(self.coords[c][0]-self.coords[d][0]) + abs(self.coords[c][1]-self.coords[d][1])) == 1:
                             contactstate.append((c,d))
-        return contactstate 
+        return contactstate
 
 
     def grow(self):
@@ -129,10 +128,10 @@ class Chain:
         self.vec.append(0)
 
         # ... update the coords
-        i = len(self.coords) - 1 
+        i = len(self.coords) - 1
         last_coord = self.coords[i]
         app_coord = (last_coord[0],last_coord[1]+1)
-        self.coords.append(app_coord) 
+        self.coords.append(app_coord)
 
         # ... update the viability
         if self.coords.count(app_coord) > 1:
@@ -154,26 +153,26 @@ class Chain:
         "3" vectors, this process is done recursively.
 
         Example:
-            [0,0,0,0] --> [0,0,0,1] 
-            [0,0,1,2] --> [0,0,1,3] 
-            [0,1,0,3] --> [0,1,1] 
+            [0,0,0,0] --> [0,0,0,1]
+            [0,0,1,2] --> [0,0,1,3]
+            [0,1,0,3] --> [0,1,1]
             [0,3,3,3] --> [1]
- 
+
         This operation is very useful for enumerating the full space of chain conformations.
         shift()  will also update the coords and the viability, accordingly.
 
         RETURN VALUES
-  
+
             returns 1 if its the last possible "shift" --> i.e. if it's all 3's, the search is done
             returns 0 otherwise
         """
 
-        # pop off all the trailing 3's		
+        # pop off all the trailing 3's
         while self.lastvec() == 3:
             self.vec.pop()	# update vec
             self.coords.pop()	# update coords
 
-            ### update viability	
+            ### update viability
             self.viable = 1
             for c in self.coords:
                 if self.coords.count(c) > 1:
@@ -186,10 +185,10 @@ class Chain:
 
         i = len(self.vec) - 1	# the last vec index
 
-        # Otherwise, increment the remaining non-"3" elements  
+        # Otherwise, increment the remaining non-"3" elements
         if self.vec[i] == 0:
-            # update vec 
-            self.vec[i] = self.vec[i] + 1    
+            # update vec
+            self.vec[i] = self.vec[i] + 1
             # update coords: rotate "up" to "right"
             self.coords[i+1] = (self.coords[i+1][0] + 1,self.coords[i+1][1] - 1)
             # update viability
@@ -198,8 +197,8 @@ class Chain:
                 self.viable = 0
 
         elif self.vec[i] == 1:
-            # update vec 
-            self.vec[i] = self.vec[i] + 1    
+            # update vec
+            self.vec[i] = self.vec[i] + 1
             # update coords: rotate "right" to "down"
             self.coords[i+1] = (self.coords[i+1][0] - 1,self.coords[i+1][1] - 1)
             # update viability
@@ -208,8 +207,8 @@ class Chain:
                 self.viable = 0
 
         else:	# i.e. self.vec[i] == 2:
-            # update vec 
-            self.vec[i] = self.vec[i] + 1    
+            # update vec
+            self.vec[i] = self.vec[i] + 1
             # update coords: rotate "down" to "left"
             self.coords[i+1] = (self.coords[i+1][0] - 1,self.coords[i+1][1] + 1)
             # update viability
@@ -228,14 +227,14 @@ class Chain:
 
         nonsym() returns 1 if the vec list is non-symmetric, 0 otherwise
         """
-        
+
         if len(self.vec) > 0:
             # walk along chain until you get to the first non '0' vec:
             i = 0
             while (self.vec[i] == 0) & (i < len(self.vec) - 1) :
                 i = i + 1
-            if self.vec[0] == 0:	
-                if (self.vec[i] == 1) | (self.vec[i] == 0): 	
+            if self.vec[0] == 0:
+                if (self.vec[i] == 1) | (self.vec[i] == 0):
                     return(1)
 
         return(0)
